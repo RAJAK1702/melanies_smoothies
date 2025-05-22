df1c8de819a04de6b2f46cab3cdc6caf9eeb6c11
RAJAK1702 <raj.ak147@gmail.com>
2025-05-22 13:55:58 +0200

    Update streamlit_app.py
    converting snowpark to pandas

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,9 +17,13 @@ name_on_order = st.text_input('Name on the smoothie:')
 st.write("""Name on the order is:""" + name_on_order)
 
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NMAE'),col('SEARCH_ON'))
-st.dataframe(data=my_dataframe, use_container_width=True)
-st.stop()
+#st.dataframe(data=my_dataframe, use_container_width=True)
+#st.stop()
 
+#Convert the snowpark dataframe to pandas dataframe so that we can use the LOC function
+pd_df=my_dataframe.to_pandas()
+st.dataframe(pd_df)
+st.stop()
 
 
 ingredients_list = st.multiselect(
